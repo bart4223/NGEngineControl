4adec50f78a3269466f723ad68d9818d3fa1bb93
Nils <nilsgrimmer@gmx.de>
2021-05-29 16:15:04 +0200

    Change Analog-Pins

--- src/NGJointControl.h
+++ src/NGJointControl.h
@@ -19,9 +19,9 @@
 #define JOINT_0             0
 #define JOINT_0_TRANSDUCER  3
 #define JOINT_1             1
-#define JOINT_1_TRANSDUCER  4
+#define JOINT_1_TRANSDUCER  1
 #define JOINT_2             2
-#define JOINT_2_TRANSDUCER  5
+#define JOINT_2_TRANSDUCER  2
 
 #define NONAME              ""
 #define TRANSDUCERTHRESHOLD  3
