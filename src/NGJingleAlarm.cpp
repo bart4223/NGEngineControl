481e81cb4e1138986659ef1d18ad815374b01855
Nils <nilsgrimmer@gmx.de>
2021-09-26 12:02:48 +0200

    Jingle Alarm

---
+++ src/NGJingleAlarm.cpp
@@ -0,0 +1,39 @@
+//
+//  NGJingleAlarm.cpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 26.09.21.
+//
+
+#include "NGJingleAlarm.h"
+
+NGJingleAlarm::NGJingleAlarm() {
+    _create();
+}
+
+void NGJingleAlarm::_create() {
+    NGCustomJingle::_create();
+    _length = 8;
+}
+
+int NGJingleAlarm::getDefaultTempo() {
+    return 110;
+}
+
+int NGJingleAlarm::getTune() {
+    int res = NO_NOTE;
+    if (_indexTune < _length) {
+        res = _tune[_indexTune];
+        _indexTune++;
+    }
+    return res;
+}
+
+int NGJingleAlarm::getBeat() {
+    int res = NO_NOTE;
+    if (_indexBeat < _length) {
+        res = _beat[_indexBeat];
+        _indexBeat++;
+    }
+    return res;
+}
---
+++ src/NGJingleAlarm.h
@@ -0,0 +1,38 @@
+//
+//  NGJingleAlarm.h
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 26.09.21.
+//
+
+#ifndef NGJingleAlarm_h
+#define NGJingleAlarm_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <NGCustomJingle.h>
+
+class NGJingleAlarm : public NGCustomJingle {
+
+private:
+    int _tune[8] = { NOTE_D5, NOTE_E5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_E5 };
+    int _beat[8] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+    
+protected:
+    void _create();
+    
+public:
+    NGJingleAlarm();
+    
+    int getDefaultTempo();
+    
+    int getTune();
+    
+    int getBeat();
+};
+
+#endif /* NGJingleAlarm_h */
