ff5b6328887a79e8fcf2219f9fdd0e2336275b73
Nils <nilsgrimmer@gmx.de>
2021-09-28 14:00:07 +0200

    Flashing Light

---
+++ src/NGFlashingLight.cpp
@@ -0,0 +1,40 @@
+//
+//  NGFlashingLight.cpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 28.09.21.
+//
+
+#include "NGFlashingLight.h"
+
+NGFlashingLight::NGFlashingLight(byte pinFlashingLight) {
+    _create(pinFlashingLight, DEFFLASHINGINTERVAL);
+}
+
+NGFlashingLight::NGFlashingLight(byte pinFlashingLight, int interval) {
+    _create(pinFlashingLight, interval);
+}
+
+NGFlashingLight::_create(byte pinFlashingLight, int interval) {
+    _pinFlashingLight = pinFlashingLight;
+    _interval = interval;
+    _flashing = false;
+}
+
+void NGFlashingLight::initialize() {
+    pinMode(_pinFlashingLight, OUTPUT);
+    digitalWrite(_pinFlashingLight, LOW);
+}
+
+void NGFlashingLight::processingLoop() {
+    unsigned long m = millis();
+    if (m - _millis >= _interval) {
+        _flashing = !_flashing;
+        if (_flashing) {
+            digitalWrite(_pinFlashingLight, HIGH);
+        } else {
+            digitalWrite(_pinFlashingLight, LOW);
+        }
+        _millis = m;
+    }
+}
---
+++ src/NGFlashingLight.h
@@ -0,0 +1,40 @@
+//
+//  NGFlashingLight.h
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 28.09.21.
+//
+
+#ifndef NGFlashingLight_h
+#define NGFlashingLight_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#define DEFFLASHINGINTERVAL 1000
+
+class NGFlashingLight {
+    
+private:
+    byte _pinFlashingLight;
+    int _interval;
+    unsigned long _millis;
+    bool _flashing;
+    
+protected:
+    _create(byte pinFlashingLight, int interval);
+    
+public:
+    NGFlashingLight(byte pinFlashingLight);
+    
+    NGFlashingLight(byte pinFlashingLight, int interval);
+    
+    void initialize();
+    
+    void processingLoop();
+};
+
+#endif /* NGFlashingLight_h */
