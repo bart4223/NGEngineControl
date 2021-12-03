54a7f6bd3223b0d9eda8da6cd16c0f4a9c8ba126
Nils <nilsgrimmer@gmx.de>
2021-12-03 16:18:35 +0100

    Test

--- examples/first/first.ino
+++ examples/first/first.ino
@@ -1,12 +1,14 @@
 const int led = 13;
 const int sleep = 1000;
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 }
 
 void loop() {
   digitalWrite(led, HIGH);
+  Serial.println("Hello world");
   delay(sleep);
   digitalWrite(led, LOW);
   delay(sleep);
