e9063359aa5aba01930bd3f9cf447a923f1bbdf5
Nils <nilsgrimmer@gmx.de>
2022-07-07 18:15:09 +0200

    New Test Level

--- examples/enginecontrol/complex/complex.ino
+++ examples/enginecontrol/complex/complex.ino
@@ -6,38 +6,58 @@ NGEngineControl engine0 = NGEngineControl(ENGINE_0);
 
 long duration;
 int testLevel = 0;
-bool startTest = true;
+bool beginTest = true;
 
 void setup() {
   engine0.initialize();
-  engine0.setSpeed(255,3000);
-  duration = millis();
 }
 
 void loop() {
   if (testLevel <= MAXTESTLEVEL) {
     switch(testLevel) {
       case 0:
-        if (startTest) {
+        if (beginTest) {
           beginTestLevel();
+          engine0.setSpeed(255,3000);
           engine0.run(edForward);
         }
-        if ((millis() - duration) >= 3000) {
+        if ((millis() - duration) >= 6000) {
           engine0.stop(2000);
-          endTestLevel();
+          endTestLevel(3000);
         }
         break;
       case 1:
-        if (startTest) {
+        if (beginTest) {
           beginTestLevel();
+          engine0.setSpeed(255,3000);
           engine0.run(edBackward);
         }
-        if ((millis() - duration) >= 3000) {
+        if ((millis() - duration) >= 6000) {
           engine0.stop(2000);
-          endTestLevel();
+          endTestLevel(3000);
         }
         break;
+      case 2:
+        if (beginTest) {
+          beginTestLevel();
+          engine0.setSpeed(255);
+          engine0.run(edForward);
+        }
+        if ((millis() - duration) >= 10000) {
+          engine0.stop();
+          endTestLevel(3000);
+        }
+        break;
+      default:
+        char log[100];
+        sprintf(log, "==>Test Level %d unknown!", testLevel);
+        Serial.println(log);
+        testLevel++;
+        break;
     }
+  } else if (testLevel == MAXTESTLEVEL + 1) {
+        Serial.println(">>>Tests finished");
+        testLevel++;
   }
 }
 
@@ -45,15 +65,17 @@ void beginTestLevel() {
   char log[100];
   sprintf(log, "==< Begin Test Level %d", testLevel);
   Serial.println(log);
-  startTest = false;
-  
+  beginTest = false;
+  duration = millis();
 }
 
-void endTestLevel() {
+void endTestLevel(int wait) {
   char log[100];
   sprintf(log, ">== End Test Level %d", testLevel);
   Serial.println(log);
   testLevel++;
-  startTest = true;
-  duration = millis();
+  beginTest = true;
+  if (wait > 0) {
+    delay(wait);
+  }
 }
