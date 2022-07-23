ca115bdc042d7ded93a54d3b65f687508fb3ec94
Nils <nilsgrimmer@gmx.de>
2022-07-23 18:51:20 +0200

    Image

--- src/NGCurrentMeasurementUnitControl.h
+++ src/NGCurrentMeasurementUnitControl.h
@@ -17,7 +17,7 @@
 #include <NGCustomUnitControl.h>
 #include <NGCurrentSensor.h>
 
-#define _VERSION "1.1"
+#define _VERSION "1.2"
 #define VERSION (char*)_VERSION
 
 #define MAXCURRENTSENSORCOUNT 3
