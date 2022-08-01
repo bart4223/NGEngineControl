3881c50bf977066b7a6a14765ac7af2c976fcb26
Nils <nilsgrimmer@gmx.de>
2022-08-01 20:06:53 +0200

    Speedy

--- projects/motoman/centralunit/centralunit.ino
+++ projects/motoman/centralunit/centralunit.ino
@@ -49,78 +49,78 @@ NGCentralUnitControl unitCentral = NGCentralUnitControl(CENTRAL);
 IRrecv _irrecv(IRREMOTE);
 
 void setup() {
-    _irrecv.enableIRIn();
-    setGlobalUnit(&unitCentral);
-    unitCentral.registerNotification(&notificationSerial);
-    #if (PROD == true)
-    unitCentral.registerNotification(&notificationLCD);
-    #endif
-    unitCentral.registerUnit(TOOL, TOOLADDRESS);
-    unitCentral.registerComponent(ctJoint, TOOL, BASE);
-    unitCentral.registerComponent(ctJoint, TOOL, SHOULDER, SHOULDERSTEPWIDTH);
-    unitCentral.registerComponent(ctJoint, TOOL, ELBOW, ELBOWSTEPWIDTH);
-    unitCentral.registerComponent(ctGripper, TOOL, GRIPPER);
-    #if (Dance == true)
-    int mpDance = unitCentral.registerMotionProfile(DANCE, TOOL);
-    int mpcDanceGripper = unitCentral.addMotionProfileComponent(mpDance, ctGripper, GRIPPER);
-    int mpcDanceShoulder = unitCentral.addMotionProfileComponent(mpDance, ctJoint, SHOULDER);
-    int mpcDanceElbow = unitCentral.addMotionProfileComponent(mpDance, ctJoint, ELBOW);
-    int mpcDanceBase = unitCentral.addMotionProfileComponent(mpDance, ctJoint, BASE);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceGripper, CGRIPPERRELEASE, 4000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceElbow, 800, 2000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceBase, 850, 3000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 400, 2000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceBase, 750, 3000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceElbow, 400, 2000);
-    unitCentral.addMotionProfileItem(mpDance, mpcDanceGripper, CGRIPPERGRIP, 4000);
-    #endif
-    #if (Dude == true)
-    int mpDude = unitCentral.registerMotionProfile(DUDE, TOOL, false);
-    int mpcDudeGripper = unitCentral.addMotionProfileComponent(mpDude, ctGripper, GRIPPER);
-    int mpcDudeShoulder = unitCentral.addMotionProfileComponent(mpDude, ctJoint, SHOULDER);
-    int mpcDudeElbow = unitCentral.addMotionProfileComponent(mpDude, ctJoint, ELBOW);
-    int mpcDudeBase = unitCentral.addMotionProfileComponent(mpDude, ctJoint, BASE);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 750, 3000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERRELEASE, 4000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 400, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERGRIP, 4000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 850, 3000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 400, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERRELEASE, 4000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 800, 3000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERGRIP, 4000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 500, 2000);
-    unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 700, 2000);
-    #endif
-    unitCentral.registerUnit(MOTION, MOTIONADDRESS);
-    unitCentral.registerIRRemoteFunction(ftMenu, IRP_APPLE, IRA_APPLE, IRC_APPLE_MENU);
-    unitCentral.registerIRRemoteFunction(ftLeft, IRP_APPLE, IRA_APPLE, IRC_APPLE_LEFT);
-    unitCentral.registerIRRemoteFunction(ftRight, IRP_APPLE, IRA_APPLE, IRC_APPLE_RIGHT);
-    unitCentral.registerIRRemoteFunction(ftUp, IRP_APPLE, IRA_APPLE, IRC_APPLE_UP);
-    unitCentral.registerIRRemoteFunction(ftDown, IRP_APPLE, IRA_APPLE, IRC_APPLE_DOWN);
-    unitCentral.registerIRRemoteFunction(ftOK, IRP_APPLE, IRA_APPLE, IRC_APPLE_OK);
-    unitCentral.registerIRRemoteFunction(ftPlay, IRP_APPLE, IRA_APPLE, IRC_APPLE_PLAY);
-    unitCentral.initialize();
-    #if (PROD == false)
-    unitCentral.setWorkMode(wmObserveMemory);
-    #endif
-    unitCentral.clearInfo();
+  _irrecv.enableIRIn();
+  setGlobalUnit(&unitCentral);
+  unitCentral.registerNotification(&notificationSerial);
+  #if (PROD == true)
+  unitCentral.registerNotification(&notificationLCD);
+  #endif
+  unitCentral.registerUnit(TOOL, TOOLADDRESS);
+  unitCentral.registerComponent(ctJoint, TOOL, BASE);
+  unitCentral.registerComponent(ctJoint, TOOL, SHOULDER, SHOULDERSTEPWIDTH);
+  unitCentral.registerComponent(ctJoint, TOOL, ELBOW, ELBOWSTEPWIDTH);
+  unitCentral.registerComponent(ctGripper, TOOL, GRIPPER);
+  #if (Dance == true)
+  int mpDance = unitCentral.registerMotionProfile(DANCE, TOOL);
+  int mpcDanceGripper = unitCentral.addMotionProfileComponent(mpDance, ctGripper, GRIPPER);
+  int mpcDanceShoulder = unitCentral.addMotionProfileComponent(mpDance, ctJoint, SHOULDER);
+  int mpcDanceElbow = unitCentral.addMotionProfileComponent(mpDance, ctJoint, ELBOW);
+  int mpcDanceBase = unitCentral.addMotionProfileComponent(mpDance, ctJoint, BASE);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceGripper, CGRIPPERRELEASE, 4000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceElbow, 800, 2000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceBase, 850, 3000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 400, 2000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceBase, 750, 3000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceShoulder, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceElbow, 400, 2000);
+  unitCentral.addMotionProfileItem(mpDance, mpcDanceGripper, CGRIPPERGRIP, 4000);
+  #endif
+  #if (Dude == true)
+  int mpDude = unitCentral.registerMotionProfile(DUDE, TOOL, false);
+  int mpcDudeGripper = unitCentral.addMotionProfileComponent(mpDude, ctGripper, GRIPPER);
+  int mpcDudeShoulder = unitCentral.addMotionProfileComponent(mpDude, ctJoint, SHOULDER);
+  int mpcDudeElbow = unitCentral.addMotionProfileComponent(mpDude, ctJoint, ELBOW);
+  int mpcDudeBase = unitCentral.addMotionProfileComponent(mpDude, ctJoint, BASE);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 750, 3000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERRELEASE, 4000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 400, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERGRIP, 4000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 850, 3000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 400, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERRELEASE, 4000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 600, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 800, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeBase, 800, 3000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeGripper, CGRIPPERGRIP, 4000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeShoulder, 500, 2000);
+  unitCentral.addMotionProfileItem(mpDude, mpcDudeElbow, 700, 2000);
+  #endif
+  unitCentral.registerUnit(MOTION, MOTIONADDRESS);
+  unitCentral.registerIRRemoteFunction(ftMenu, IRP_APPLE, IRA_APPLE, IRC_APPLE_MENU);
+  unitCentral.registerIRRemoteFunction(ftLeft, IRP_APPLE, IRA_APPLE, IRC_APPLE_LEFT);
+  unitCentral.registerIRRemoteFunction(ftRight, IRP_APPLE, IRA_APPLE, IRC_APPLE_RIGHT);
+  unitCentral.registerIRRemoteFunction(ftUp, IRP_APPLE, IRA_APPLE, IRC_APPLE_UP);
+  unitCentral.registerIRRemoteFunction(ftDown, IRP_APPLE, IRA_APPLE, IRC_APPLE_DOWN);
+  unitCentral.registerIRRemoteFunction(ftOK, IRP_APPLE, IRA_APPLE, IRC_APPLE_OK);
+  unitCentral.registerIRRemoteFunction(ftPlay, IRP_APPLE, IRA_APPLE, IRC_APPLE_PLAY);
+  unitCentral.initialize();
+  #if (PROD == false)
+  unitCentral.setWorkMode(wmObserveMemory);
+  #endif
+  unitCentral.clearInfo();
 }
 
 void loop() {
-    if (_irrecv.decode()) {
-      unitCentral.setIRRemoteData(_irrecv.decodedIRData.protocol,_irrecv.decodedIRData.address,_irrecv.decodedIRData.command);
-      _irrecv.resume();
-    }
-    unitCentral.processingLoop();
+  if (_irrecv.decode()) {
+    unitCentral.setIRRemoteData(_irrecv.decodedIRData.protocol,_irrecv.decodedIRData.address,_irrecv.decodedIRData.command);
+    _irrecv.resume();
+  }
+  unitCentral.processingLoop();
 }
---
+++ projects/speedy/speedy/speedy.ino
@@ -0,0 +1,39 @@
+#define PROD false  //false,true
+
+#include <IRremote.h>
+#if (PROD == false)
+#include <NGSerialNotification.h>
+#endif
+#include <NGCentralUnitControl.h>
+
+#define _CENTRAL      "Speedy"
+#define CENTRAL       (char*)_CENTRAL
+
+#define IRREMOTE    11
+
+#if (PROD == false)
+NGSerialNotification notificationSerial = NGSerialNotification();
+#endif
+NGCentralUnitControl unitCentral = NGCentralUnitControl(CENTRAL);
+IRrecv _irrecv(IRREMOTE);
+
+void setup() {
+  _irrecv.enableIRIn();
+  setGlobalUnit(&unitCentral);
+  #if (PROD == false)
+  unitCentral.registerNotification(&notificationSerial);
+  #endif
+  unitCentral.initialize();
+  #if (PROD == false)
+  unitCentral.setWorkMode(wmObserveMemory);
+  #endif
+  unitCentral.clearInfo();
+}
+
+void loop() {
+  if (_irrecv.decode()) {
+    unitCentral.setIRRemoteData(_irrecv.decodedIRData.protocol,_irrecv.decodedIRData.address,_irrecv.decodedIRData.command);
+    _irrecv.resume();
+  }
+  unitCentral.processingLoop();
+}
