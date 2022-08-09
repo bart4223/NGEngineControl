2ac95d484968e197ce77f7db946eae7cc951069b
Nils <nilsgrimmer@gmx.de>
2022-08-09 20:48:41 +0200

    Full pin usage

--- examples/wirelessreceiver/wirelessreceiver.ino
+++ examples/wirelessreceiver/wirelessreceiver.ino
@@ -1,19 +1,31 @@
 #include <NGSimpleWirelessReceiver.h>
+#include <NGJingleBeep.h>
 #include <NGJingleGorilla.h>
+#include <NGJingleSuperMarioShort.h>
+#include <NGJingleHelloDude.h>
 #include <NGSoundMachine.h>
 
-#define PIN0 7
+#define PINONE   7
+#define PINTWO   8
+#define PINTHREE 9
+#define PINFOUR 10
 #define DELAY 500
 
 NGSimpleWirelessReceiver swr = NGSimpleWirelessReceiver();
 NGSoundMachine sm = NGSoundMachine();
-int jingleBeep;
+int jingleOne, jingleTwo, jingleThree, jingleFour;
 
 void setup() {
   Serial.begin(9600);
-  swr.registerCallback(PIN0, swrmHIGH, &fire, DELAY);
+  swr.registerCallback(PINONE, swrmHIGH, &fireOne, DELAY);
+  swr.registerCallback(PINTWO, swrmHIGH, &fireTwo, DELAY);
+  swr.registerCallback(PINTHREE, swrmHIGH, &fireThree, DELAY);
+  swr.registerCallback(PINFOUR, swrmHIGH, &fireFour, DELAY);
   swr.initialize();
-  jingleBeep = sm.registerJingle(new NGJingleGorilla);
+  jingleOne = sm.registerJingle(new NGJingleBeep);
+  jingleTwo = sm.registerJingle(new NGJingleGorilla);
+  jingleThree = sm.registerJingle(new NGJingleSuperMarioShort);
+  jingleFour = sm.registerJingle(new NGJingleHelloDude);
   sm.initialize();
 }
 
@@ -21,7 +33,22 @@ void loop() {
   swr.processingLoop();
 }
 
-void fire() {
-  Serial.println("Fire");
-  sm.play(jingleBeep);
+void fireOne() {
+  Serial.println("Fire One");
+  sm.play(jingleOne);
+}
+
+void fireTwo() {
+  Serial.println("Fire Two");
+  sm.play(jingleTwo);
+}
+
+void fireThree() {
+  Serial.println("Fire Three");
+  sm.play(jingleThree);
+}
+
+void fireFour() {
+  Serial.println("Fire Four");
+  sm.play(jingleFour);
 }
