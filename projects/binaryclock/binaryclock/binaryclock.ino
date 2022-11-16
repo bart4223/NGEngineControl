f8d6f887d8b8414757b96cdc882573ecaf8a488e
Nils <nilsgrimmer@gmx.de>
2022-11-16 18:04:26 +0100

    Optimization

--- projects/binaryclock/binaryclock/binaryclock.ino
+++ projects/binaryclock/binaryclock/binaryclock.ino
@@ -78,6 +78,16 @@ byte snow[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 long lastSnow = 0;
 
+byte bell[][2] = {{3, 0}, {4, 0},
+  {2, 1}, {3, 1}, {4, 1}, {5, 1},
+  {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2},
+  {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {6, 3},
+  {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4},
+  {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}, {6, 5}, {7, 5},
+  {2, 6}, {3, 6}, {4, 6}, {5, 6},
+  {3, 7}, {4, 7}
+};
+
 colorRGB heartColor;
 byte heartBeat = HEARTBEATBASE;
 bool heartDirectionUp = true;
@@ -189,7 +199,10 @@ void SimpleKeypadCallback(byte id) {
       }
       break;
     case KEYSOUNDID:
+      cdm.clear();
+      cdm.drawImage(bell, COLOR_RED, sizeof(bell) / sizeof(bell[0]));
       sm.play(0);
+      cdm.clear();
       break;
     default:
       Serial.println(id);
