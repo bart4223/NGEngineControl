69b4828b02f8b636a4bea66777aa377d06fb570e
Nils <nilsgrimmer@gmx.de>
2023-09-29 11:39:06 +0200

    Anchorman

---
+++ src/NGAnchormanUnitControl.cpp
@@ -0,0 +1,8 @@
+//
+//  NGAnchormanUnitControl.cpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 25.09.23.
+//
+
+#include "NGAnchormanUnitControl.hpp"
---
+++ src/NGAnchormanUnitControl.h
@@ -0,0 +1,13 @@
+//
+//  NGAnchormanUnitControl.hpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 25.09.23.
+//
+
+#ifndef NGAnchormanUnitControl_hpp
+#define NGAnchormanUnitControl_hpp
+
+#include <stdio.h>
+
+#endif /* NGAnchormanUnitControl_hpp */
