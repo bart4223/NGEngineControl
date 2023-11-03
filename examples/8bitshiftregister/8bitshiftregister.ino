8576ea588eb372afe348c0df1bb017fb80d45de3
Nils <nilsgrimmer@gmx.de>
2023-11-03 15:14:43 +0100

    Move to core

--- examples/8bitshiftregister/8bitshiftregister.ino
+++ examples/8bitshiftregister/8bitshiftregister.ino
@@ -1,23 +1,41 @@
-#include <NGMemoryObserver.h>
+#include <NGCommon.h>
 #include <NG8BitShiftRegister.h>
 
-#define LATCHPIN  7 // 7, 4
-#define CLOCKPIN  8 // 8, 5
-#define DATAPIN   6 // 6, 3
+#ifdef NG_PLATFORM_AVR
+#include <NGMemoryObserver.h>
+#endif
+
+#define LATCHPIN  4 // 7, 4
+#define CLOCKPIN  5 // 8, 5
+#define DATAPIN   3 // 6, 3
 
 #define DELAY 1000
+//#define RANDOMIZE
 
 NG8BitShiftRegister sr = NG8BitShiftRegister(LATCHPIN, CLOCKPIN, DATAPIN);
 
 void setup() {
   sr.initialize();
+  #ifdef RANDOMIZE
   sr.setValue(random(0, 255));
+  #else
+  sr.setValue(1);
+  #endif
 }
 
 void loop() {
+  #ifdef NG_PLATFORM_AVR
   observeMemory(DELAY);
+  #endif
+  #ifdef NG_PLATFORM_ARM
+  delay(DELAY);
+  #endif
   if (sr.getValue() == 128) {
+    #ifdef RANDOMIZE
     sr.setValue(random(0, 255));
+    #else
+    sr.setValue(1);
+    #endif
   } else {
     sr.shiftValue();
   }
