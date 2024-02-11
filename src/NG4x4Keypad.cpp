6af8e04b8ebfd674f6980974c8dbdb6c1570c51a
Nils <nilsgrimmer@gmx.de>
2024-02-11 12:43:21 +0100

    4x4Keypad works

---
+++ src/NG4x4Keypad.cpp
@@ -0,0 +1,8 @@
+//
+//  NG4x4Keypad.cpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 10.02.24.
+//
+
+#include "NG4x4Keypad.hpp"
---
+++ src/NG4x4Keypad.h
@@ -0,0 +1,13 @@
+//
+//  NG4x4Keypad.hpp
+//  NGEngineControl
+//
+//  Created by Nils Grimmer on 10.02.24.
+//
+
+#ifndef NG4x4Keypad_hpp
+#define NG4x4Keypad_hpp
+
+#include <stdio.h>
+
+#endif /* NG4x4Keypad_hpp */
