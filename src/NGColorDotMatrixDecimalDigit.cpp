56818f6b3ce1c101e37a9b914aaeda96753fa971
Nils <nilsgrimmer@gmx.de>
2024-04-23 16:38:56 +0200

    Fix

--- src/NGColorDotMatrixDecimalDigit.cpp
+++ src/NGColorDotMatrixDecimalDigit.cpp
@@ -14,34 +14,34 @@ NGColorDotMatrixDecimalDigit::NGColorDotMatrixDecimalDigit(NGIPaintableComponent
 void NGColorDotMatrixDecimalDigit::setValue(byte value) {
     switch(value) {
         case 0x00:
-            _value = '0';
+            setChar('0');
             break;
         case 0x01:
-            _value = '1';
+            setChar('1');
             break;
         case 0x02:
-            _value = '2';
+            setChar('2');
             break;
         case 0x03:
-            _value = '3';
+            setChar('3');
             break;
         case 0x04:
-            _value = '4';
+            setChar('4');
             break;
         case 0x05:
-            _value = '5';
+            setChar('5');
             break;
         case 0x06:
-            _value = '6';
+            setChar('6');
             break;
         case 0x07:
-            _value = '7';
+            setChar('7');
             break;
         case 0x08:
-            _value = '8';
+            setChar('8');
             break;
         case 0x09:
-            _value = '9';
+            setChar('9');
             break;
     }
 }
