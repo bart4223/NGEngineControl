3014add18dfe088b83a3fc47343529e1821e31fe
Nils <nilsgrimmer@gmx.de>
2025-01-11 14:04:29 +0100

    Optimizations

--- src/NGMemoryObserver.h
+++ src/NGMemoryObserver.h
@@ -5,14 +5,10 @@
 //  Created by Nils Grimmer on 05.06.21.
 //
 
-#ifndef NGMemoryObservere_h
+#ifndef NGMemoryObserver_h
 #define NGMemoryObserver_h
 
-#if (ARDUINO >= 100)
 #include <Arduino.h>
-#else
-#include <WProgram.h>
-#endif
 
 #ifdef __cplusplus
 extern "C" {
