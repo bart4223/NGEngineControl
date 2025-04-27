f38f67023821e8efd4b6f6fc3987e39024493770
Nils <nilsgrimmer@gmx.de>
2025-04-27 15:27:07 +0200

    Optimizations

--- projects/littlebigclock/littlebigclock/littlebigclock.ino
+++ projects/littlebigclock/littlebigclock/littlebigclock.ino
@@ -1,22 +1,14 @@
 #define PROD false //false, true
-#define FONTZX81 //FONTDEFAULT, FONTZX81, FONTZXSPECTRUM, FONTC64
 
 #include <NGMemoryObserver.h>
+#include <NGZX81Font.h>
+#include <NGC64Font.h>
 #include <NGSimpleKeypad.h>
 #include <Visuals/NG8x8DotMatrix.h>
 #include <Specs/NGDotMatrixWatchDial.h>
 #include <Apps/NGLittleBigClockUnitControl.h>
 #if (PROD != true)
 #include <NGSerialNotification.h>
-#endif
-#ifdef FONTZX81
-#include <NGZX81Font.h>
-#endif
-#ifdef FONTZXSPECTRUM
-#include <NGZXSpectrumFont.h>
-#endif
-#ifdef FONTC64
-#include <NGC64Font.h>
 #endif
 
 #define _LITTLEBIGCLOCK       "Little Big Clock"
@@ -32,6 +24,8 @@
 #define KEYMINUTEID   42
 #define KEYHOURPIN    15
 #define KEYHOURID     43
+#define KEYFONTPIN    16
+#define KEYFONTID     44
 
 #define KEYDELAY     500
 
@@ -43,15 +37,9 @@ NGColorDotMatrixDecimalDigit *ddHourTens = new NGColorDotMatrixDecimalDigit(cdm)
 NGColorDotMatrixDecimalDigit *ddMinuteOne = new NGColorDotMatrixDecimalDigit(cdm);
 NGColorDotMatrixDecimalDigit *ddMinuteTens = new NGColorDotMatrixDecimalDigit(cdm);
 NGDotMatrixWatchDial wd = NGDotMatrixWatchDial(cdm);
-#ifdef FONTZX81
-NGCustomFont *font = new NGZX81Font();
-#endif
-#ifdef FONTZXSPECTRUM
-NGCustomFont *font = new NGZXSpectrumFont();
-#endif
-#ifdef FONTC64
-NGCustomFont *font = new NGC64Font();
-#endif
+NGCustomFont *fontZX81 = new NGZX81Font();
+NGCustomFont *fontC64 = new NGC64Font();
+byte fontIndex = 1;
 
 NGLittleBigClockUnitControl unitLittleBigClock = NGLittleBigClockUnitControl(LITTLEBIGCLOCK);
 #if (PROD != true)
@@ -67,16 +55,15 @@ void setup() {
   skp.registerCallback(&keypadCallback);
   skp.registerKey(KEYMINUTEPIN, KEYMINUTEID, KEYDELAY);
   skp.registerKey(KEYHOURPIN, KEYHOURID, KEYDELAY);
+  skp.registerKey(KEYFONTPIN, KEYFONTID, KEYDELAY);
   skp.initialize();
   // DotMatrix
   cdm->initialize(DOTMATRIXBRIGHTNESS);
   // Decimal Digits
-  #ifndef FONTDEFAULT
-  ddHourTens->setFont(font);
-  ddHourOne->setFont(font);
-  ddMinuteTens->setFont(font);
-  ddMinuteOne->setFont(font);
-  #endif
+  ddHourTens->setFont(fontZX81);
+  ddHourOne->setFont(fontZX81);
+  ddMinuteTens->setFont(fontZX81);
+  ddMinuteOne->setFont(fontZX81);
   ddHourTens->setPosX(0);
   ddHourOne->setPosX(8);
   ddMinuteTens->setPosX(16);
@@ -112,9 +99,31 @@ void keypadCallback(byte id) {
   switch (id) {
     case KEYMINUTEID:
       rtc.incrementMinute();
+      unitLittleBigClock.invalidWatchDial();
       break;
     case KEYHOURID:
       rtc.incrementHour();
-      break;    
+      unitLittleBigClock.invalidWatchDial();
+      break;  
+    case KEYFONTID:
+      NGCustomFont *font = nullptr;
+      fontIndex++;
+      if (fontIndex > 2) {
+        fontIndex = 0;
+      }
+      switch (fontIndex) {
+        case 1:
+          font = fontZX81;
+          break;
+        case 2:
+          font = fontC64;
+          break;
+      }
+      ddHourTens->setFont(font);
+      ddHourOne->setFont(font);
+      ddMinuteTens->setFont(font);
+      ddMinuteOne->setFont(font);
+      unitLittleBigClock.invalidWatchDial();
+      break;  
   }
 }
