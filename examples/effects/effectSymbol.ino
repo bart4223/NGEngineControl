46e3123c0a98eb0d14feb88a2a6989ee308e6fec
Nils <nilsgrimmer@gmx.de>
2025-08-30 10:52:02 +0200

    New effect with symbol font test

---
+++ examples/effects/effectSymbol.ino
@@ -0,0 +1,57 @@
+#define DOTMATRIX //LEDSTRIP100, LEDSTRIP256, DOTMATRIX
+
+#include <NGMemoryObserver.h>
+#include <Visuals/NGColorLEDStrip.h>
+#include <Visuals/NG8x8DotMatrix.h>
+#include <NGColorDotMatrixEffectText.h>
+#include <Fonts/NGWingDingsFont.h>
+
+#ifdef LEDSTRIP256
+#define LEDSTRIPPIN           8
+#define LEDSTRIPPIXELS      256
+#define LEDSTRIPROWS         16
+#define LEDSTRIPBRIGHTNESS 0.05
+#endif
+#ifdef LEDSTRIP100
+#define LEDSTRIPPIN           8
+#define LEDSTRIPPIXELS      100
+#define LEDSTRIPROWS         10
+#define LEDSTRIPBRIGHTNESS 0.05
+#endif
+#ifdef DOTMATRIX
+#define DOTMATRIXBRIGHTNESS 0.05
+#define DOTMATRIXCOUNT  4
+#define DOTMATRIXROWS   8
+#define DOTMATRIXCOLS  32
+#endif
+
+#ifdef LEDSTRIP256
+NGColorLEDStrip *cdm = new NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS, lskUpDownAlternate);
+#endif
+#ifdef LEDSTRIP100
+NGColorLEDStrip *cdm = new NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS);
+#endif
+#ifdef DOTMATRIX
+NG8x8DotMatrix *cdm = new NG8x8DotMatrix(DOTMATRIXCOUNT, DOTMATRIXROWS, DOTMATRIXCOLS, dmamInverse);
+#endif
+
+NGCustomFont *font = new NGWingDingsFont();
+NGColorDotMatrixEffectText *effect = new NGColorDotMatrixEffectText(cdm, COLOR_RED, font);
+
+void setup() {
+  observeMemory(0);
+  #ifdef DOTMATRIX
+  cdm->initialize(DOTMATRIXBRIGHTNESS);
+  #else
+  cdm->initialize(LEDSTRIPBRIGHTNESS);
+  #endif
+  effect->initialize();
+  effect->setPosition(8, 0);
+  //effect->setDelay(100);
+  effect->setText("S");
+  observeMemory(0);
+}
+
+void loop() {
+  effect->processingLoop();
+}
