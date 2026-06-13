a670c2769a5f81260256230bcb1e5bb2656ab66f
Nils <nilsgrimmer@gmx.de>
2026-06-13 21:13:35 +0200

    Optimizations

--- projects/iglasses/iglasses/iglasses.ino
+++ projects/iglasses/iglasses/iglasses.ino
@@ -8,9 +8,21 @@
 #define LEDSTRIPPIXELS  24
 #define LEDSTRIPROWS     2
 
-#define BRIGHTNESS     0.5
+#define KEYMODEPIN     9
+#define KEYMODEID     42
+#define KEYDELAY     500
 
-#define TESTBRIGHTNESS  0.05
+#define BRIGHTNESS_LOW_PIN    7
+#define BRIGHTNESS_MEDIUM_PIN 6
+#define BRIGHTNESS_HIGH_PIN   5
+#define BRIGHTNESS_ULTRA_PIN  4
+
+#define BRIGHTNESS_DEFAULT 0.05
+#define BRIGHTNESS_LOW     0.25
+#define BRIGHTNESS_MEDIUM  0.5
+#define BRIGHTNESS_HIGH    0.75
+#define BRIGHTNESS_ULTRA   1.00
+
 #define TESTMODEDELAY     20
 #define TESTDELAY        100
 #define TESTRUNS           1
@@ -18,24 +30,46 @@
 #if (PROD == false)
 NGSerialNotification serialNotification = NGSerialNotification();
 #endif
+NGSimpleKeypad skp = NGSimpleKeypad();
 NGColorLEDStrip cls = NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS);
 
 void setup() {
   observeMemory(0);
+  // Keypad
+  skp.registerCallback(&keypadCallback);
+  skp.registerKey(KEYMODEPIN, KEYMODEID, KEYDELAY);
+  // Color LED Strip
   #if (PROD == false)
   cls.setTestModeDelay(TESTMODEDELAY);
-  cls.initialize(TESTBRIGHTNESS);
+  cls.initialize(BRIGHTNESS_DEFAULT);
   for (int i = 0; i < TESTRUNS; i++) {
     cls.testSequenceStart();
     delay(TESTDELAY);
   }
   cls.testSequenceStop();
   #else
-  cls.initialize(BRIGHTNESS);
+  if (IsSwitchOn(BRIGHTNESS_ULTRA_PIN)) {
+    cls.initialize(BRIGHTNESS_ULTRA);
+  } else if (BRIGHTNESS_HIGH_PIN) {
+    cls.initialize(BRIGHTNESS_HIGH);
+  } else if (BRIGHTNESS_MEDIUM_PIN) {
+    cls.initialize(BRIGHTNESS_MEDIUM);
+  } else if (BRIGHTNESS_LOW_PIN) {
+    cls.initialize(BRIGHTNESS_LOW);
+  } else {
+    cls.initialize(BRIGHTNESS_DEFAULT);
+  }
   #endif
   observeMemory(0);
 }
 
 void loop() {
 
+}
+
+void keypadCallback(byte id) {
+  switch (id) {
+    case KEYMODEID:
+      break;
+  }
 }
